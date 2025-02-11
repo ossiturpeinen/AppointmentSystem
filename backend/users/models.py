0ef97cdd464d39e105c36a297a870ed73b118c44
Ossi Turpeinen <ossijohannesturpeinen@gmail.com>
2025-02-11 10:43:03 +0200

    Created custom user and added apps to settings

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    def __str__(self):
+        return self.email
